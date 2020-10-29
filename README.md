e1175a43aaf41c7cb5ece66d0910e48328e66b49
LuckyLuk420 <lpurmom@gmail.com>
2020-10-29 22:24:24 +0100

    added user agent to request

--- README.md
+++ README.md
@@ -3,13 +3,19 @@
 
 ## [Gewoba](https://www.gewoba.de/mieten-verwalten-kaufen-verkaufen/wohnung-mieten)
 
-A few hours in I noticed i was following a tutorial for static websites, trying to srap dynamic content.
+- A few hours in I noticed i was following a tutorial for static websites, trying to srap dynamic content.
 
-Checked XHR requests, but only requests made where for the map view
+- Checked XHR requests, but only requests made where for the map view
 
 
 ## [ImmoScout24](https://www.immobilienscout24.de/wohnen/mietwohnungen.html)
 
-The most important search filters can be made over the url	// /Suche/de/bremen/bremen/wohnung-mieten?numberofrooms=2.0-&price=-550.0&livingspace=40.0-&enteredFrom=one_step_search
+- The most important search filters can be made over the url	`/Suche/de/bremen/bremen/wohnung-mieten?numberofrooms=2.0-&price=-550.0&livingspace=40.0-&enteredFrom=one_step_search`
 
+- Requesting the site via formatted url returns captcha check
 
+- Created dictionary containing my user agent and got the search results
+
+- Successfully copied and used a Microsoft Edge 85.0 User Agent from [user-agents.net](https://user-agents.net/browsers/edge)
+
+- While searching and selecting the nodes with the relevent data i encountered a captcha again. I made probably too many (about 5) requests per minute
--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-from requests_html import HTMLSession
-
-
-# Init http session
-session = HTMLSession()
+import requests
+from bs4 import BeautifulSoup
 
 ### IMMOBILIENSCOUT24
 # URL consists of base, location selector and search query
 BASE_URL = 'https://www.immobilienscout24.de/Suche'
-LOCATION_QUERY = '/de/bremen/bremen/wohnung-mieten'
-SEARCH_QUERY = '?numberofrooms=2.0-&price=-550.0&livingspace=40.0-&enteredFrom=one_step_search'
+LOCATION_QUERY = '/de/{}/{}/wohnung-mieten'
+SEARCH_QUERY = '?numberofrooms={}-&price=-{}&livingspace={}-&enteredFrom=one_step_search'
 
-# GET request
-page = session.get(BASE_URL+LOCATION_QUERY+SEARCH_QUERY)
+# Get query input from user
+FILTERS = ['city', 'numberofrooms', 'price', 'livingspace']
+RELEVANT_FILTERS = {}
+for Filter in FILTERS:
+    if Filter == 'city':
+        RELEVANT_FILTERS[Filter] = input(Filter + ': ').lower()
+    else:
+        RELEVANT_FILTERS[Filter] = float(input(Filter + ': '))
 
-print(page.content)
+# Create user agent
+user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                            'Chrome/85.0.4183.102 Safari/537.36 Edg/85.0.564.51'}
+
+# Make URL and GET request
+URL = BASE_URL + LOCATION_QUERY.format(RELEVANT_FILTERS['city'], RELEVANT_FILTERS['city']) + SEARCH_QUERY.format(RELEVANT_FILTERS['numberofrooms'], RELEVANT_FILTERS['price'], RELEVANT_FILTERS['livingspace'])
+page = requests.get(URL, headers=user_agent)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup.text)
+
+
+# Select search results
+results = soup.find(id='resultListItems')
+
+
+
+
+
+# print(results.find('div.result-list-entry__address'))
+# print(soup)
